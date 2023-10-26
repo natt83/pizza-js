9f1bbe7a81cabe058dfec06d830a7733179240a9
natt83 <natimetal83@gmail.com>
2023-10-26 09:39:43 +0200

    ex1

--- app.js
+++ app.js
@@ -1,38 +1,49 @@
-const masa="masa";
-const salsa="salsa de tomate";
-const cebolla="cebolleta";
-let carne="pollo";
-let queso="brie";
-let mermelada="arandanos";
-let frutosSecos="nueces";
-const aceite="aceite de oliva";
-let temperatura="220";
-let horneado="25";
+const masa = "masa";
+const salsa = "salsa de tomate";
+const cebolla = "cebolleta";
+let carne = "pollo";
+let queso = "brie";
+let mermelada = "arandanos";
+let frutosSecos = "nueces";
+const aceite = "aceite de oliva";
+let temperatura = 220;
+let tiempoHorneado =25;
+
 function app() {
-    comprarmasa()
-    picar()
-    ingredientes()
-    horneado()
+  comprarmasa();
+  picar();
+  ingredientes();
+  horneado();
 }
 
-app()
+app();
 
-function comprarmasa () {
-    console.log("Paso 1: Comprar " + masa)
+function comprarmasa() {
+  console.log("Paso 1: Comprar " + masa);
 }
 
-function picar(){
-    console.log("Paso 2: picar " + cebolla)
-    console.log("Paso 3: picar " + carne)
+function picar() {
+  console.log("Paso 2: picar " + cebolla);
+  console.log("Paso 3: picar " + carne);
 }
-   
- console.log("Paso 1:extender masa de pizza");
-    console.log("Paso 2: Agregar la " + salsa);
-    console.log("Paso 3: Esparcir el " + queso)
-    console.log("Paso 4: Agregar los ingredientes");
-    let ingredientes =("pollo","brie","arandanos")
-   ingredientes.push("pollo","brie","arandanos","nueces","aceite de oliva",)
-   console.log("paso 5:hornear" + horneado )
-   console.log("temperatura" + temperatura)
+function ingredientes() {
+  console.log(
+    "Paso 4: Agregar los ingredientes:" +
+      carne +
+      " " +
+      queso +
+      " " +
+      mermelada +
+      " " +
+      frutosSecos +
+      " " +
+      aceite
+  );
+}
+function horneado() {
+  console.log("paso 5:hornear" + tiempoHorneado);
+}
 
-   console.log("¡¡la pizza esta lista!!")
+//ingredientes.push("pollo","brie","arandanos","nueces","aceite de oliva",)
+//console.log("paso 5:hornear" + horneado )
+console.log("¡¡la pizza esta lista!!");
