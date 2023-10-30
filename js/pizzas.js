46abc71dd4f8643167cda48355087662593e63cd
natt83 <natimetal83@gmail.com>
2023-10-30 10:15:56 +0100

    select pizza

--- js/pizzas.js
+++ js/pizzas.js
@@ -1,28 +1,41 @@
-function escogerPizza(pizza) {
-    var pizzaSeleccionada;
-    
-    if (pizza === 1) {
-        pizzaSeleccionada = {
-            masa: "Delgada",
+
+import PromptSync from "prompt-sync";
+
+let pizza=[];
+escogerPizza();
+ function escogerPizza() {
+    const prompt= PromptSync();
+    const pizzaSeleccionada=prompt("¿Que pizza desea?:");
+
+    if (pizzaSeleccionada == 1) {
+       console.log(pizzaSeleccionada);
+         pizza = {
+            masa: "fina",
             salsa: "Tomate",
             ingredientes: ["Pepperoni", "Champiñones", "Queso"],
             costo: 12
         };
-    } else if (pizza === 2) {
-        pizzaSeleccionada = {
+        //return pizza
+    } else if (pizzaSeleccionada == 2) {
+        console.log(pizzaSeleccionada);
+        pizza = {
             masa: "Gruesa",
             salsa: "Pesto",
             ingredientes: ["Pollo", "Tomate Seco", "Queso de Cabra"],
             costo: 14
         };
+        //return pizza
     } else {
         alert("Opción no válida");
         return;
     }
 
-    console.log("Tu pizza tiene masa " + pizzaSeleccionada.masa + " y salsa de " + pizzaSeleccionada.salsa + ".");
+ }
+    console.log("Tu pizza tiene masa " + pizza.masa + " y salsa de " + pizza.salsa + ".");
     console.log("Ingredientes:");
-    for (var i = 0; i < pizzaSeleccionada.ingredientes.length; i++) {
-        console.log("- " + pizzaSeleccionada.ingredientes[i]);
+    for (let i = 0; i < pizza.ingredientes.length; i++) {
+        console.log("- " + pizza.ingredientes[i]);
     }
-    console.log("Costo total: $" + pizzaSeleccionada.costo)};
+    
+    console.log("Costo total: $" + pizza.costo);
+    
